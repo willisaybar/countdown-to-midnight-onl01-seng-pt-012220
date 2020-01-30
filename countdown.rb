ade62b56d3fb2dc0f8d160a505d7e77a40d6c114
Willis Aybar <willisaybar@gmail.com>
2020-01-30 04:05:10 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
-#write your code here
+def countdown(new_year)
+  new_year = 10
+  while new_year > 0
+    puts "#{new_year} SECOND(S)!"
+    new_year -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
 
-def countdown
+
+
+def countdown_with_sleep(tired)
+  tired = 5
+  while tired > 0
+    puts "#{tired} SECOND(S)!"
+    sleep 5
+    tired -= 1
+  end
 end
